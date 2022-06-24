b34c0cbb432d0893e93d682c3bb7624972e815d8
letminjae <cmjj0824@naver.com>
2022-06-24 23:45:55 +0900

    UPDATE : Webpack dev server and Hot reloading

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -1,12 +1,45 @@
-const React = require('react');
-const { Component } = React
+const React = require("react");
 
 const WordRelay = () => {
-    return(
-        <>
-        <h1>hi WordRelay!</h1>
-        </>
-    );
-}
+  const [word, setWord] = React.useState("차민재");
+  const [value, setValue] = React.useState("");
+  const [answer, setAnswer] = React.useState("");
+  const ref = React.useRef(null);
+
+  const handleOnChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setAnswer("딩동댕");
+      setWord(value);
+      setValue("");
+      ref.current.focus();
+    } else {
+      setAnswer("땡");
+      setValue("");
+      ref.current.focus();
+    }
+  };
+
+  return (
+    <>
+      <h1>{word}</h1>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          ref={ref}
+          type="text"
+          placeholder="다음 낱말은?"
+          value={value}
+          onChange={handleOnChange}
+        />
+        <button>클릭!</button>
+      </form>
+      <h2>{answer}</h2>
+    </>
+  );
+};
 
 module.exports = WordRelay;
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,29 +1,50 @@
-const path = require('path');
+const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
-    name : 'wordrelay-setting',
-    mode : 'development',
-    devtool : 'eval', //빠르게하겠다
-    resolve : {
-        extensions: ['.jsx'], // entry에 확장자('./client.jsx')를 안써도 웹팩이 찾아서 entry에 넣어줌
-    },
-    
-    entry: {
-        app: ['./client'],
-    }, // 입력을 한 후,
+  name: "wordrelay-setting",
+  mode: "development",
+  devtool: "eval", //빠르게하겠다
+  resolve: {
+    extensions: [".js", ".jsx"], // entry에 확장자('./client.jsx')를 안써도 웹팩이 찾아서 entry에 넣어줌
+  },
 
-    module: {
-        rules: [{
-            test : /\.jsx?/, //jsx파일 전체를
-            loader: 'babel-loader', // 바벨로더 적용시킨다
-            options :{
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
-        }], //규칙 정하기
-    }, // 모듈을 적용시키고,
+  entry: {
+    app: ["./client"],
+  }, // 입력을 한 후,
 
-    output : {
-        path : path.join(__dirname, 'dist'),
-        filename: 'app.js'
-    }, // 출력한다
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, //jsx파일 전체를
+        loader: "babel-loader", // 바벨로더 적용시킨다
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 5% in KR", "last 2 chrome versions"], // 지원하고자하는 브라우저
+                },
+                debug: true, //디버깅 허용
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: ["react-refresh/babel"],
+        },
+      },
+    ], //규칙 정하기
+  }, // 모듈을 적용시키고,
+  plugins: [new RefreshWebpackPlugin()],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+    publicPath: "/dist/",
+  }, // 출력한다
+  devServer: {
+    devMiddleware: { publicPath: "/dist/" },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+  },
 };
