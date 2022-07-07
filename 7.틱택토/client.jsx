fe6377716f6309ecccebe9c981e83554107cf65e
letminjae <cmjj0824@naver.com>
2022-07-07 22:52:16 +0900

    Initialize Tictactoe Game with React

---
+++ 7.틱택토/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Tictactoe from './tictactoe';
+
+ReactDOM.render(<Tictactoe />, document.querySelector('#root'));
---
+++ 7.틱택토/tictactoe.jsx
@@ -0,0 +1,13 @@
+import React, { useState, memo, useEffect, useRef, useCallback, useMemo } from "react";
+
+
+const Tictactoe = memo(() => {
+
+  return (
+    <>
+    <div>틱택토</div>
+    </>
+  );
+});
+
+export default Tictactoe;
